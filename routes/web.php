054f0473dc064cc8121fdbb43a10dc5fb8510f9a
Ryotaro Yamamoto <yamatako64@gmail.com>
2025-08-12 17:53:21 +0800

    home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('managers.home');
 });
 
 Auth::routes();
